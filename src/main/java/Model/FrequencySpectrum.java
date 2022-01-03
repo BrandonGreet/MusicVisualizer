d0562c6e9daa6f41d5985bc8f84d02694794dc61
Brandon Greet <btg5907@rit.edu>
2022-01-02 21:08:05 -0500

    Use absolute value of frequency amplitude in FrequencySpectrum

--- src/main/java/Model/FrequencySpectrum.java
+++ src/main/java/Model/FrequencySpectrum.java
@@ -2,13 +2,15 @@ package Model;
 
 import Controller.Utils;
 
+import java.util.Arrays;
+
 public class FrequencySpectrum implements IAudioData {
     private double[] frequencies;
 
     public FrequencySpectrum(short[] samples) {
         double[] temp = new double[512];
         for (int i = 0; i < samples.length; i++) temp[i] = samples[i];
-        this.frequencies = Utils.FastFourierTransform(temp);
+        this.frequencies = Arrays.stream(Utils.FastFourierTransform(temp)).map(Math::abs).toArray();
     }
 
     @Override
